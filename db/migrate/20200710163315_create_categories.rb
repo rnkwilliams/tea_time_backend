9cf5c11d3a479d4c05c6dcc30ed3fb7fc625da08
rnkwilliams <rnkwilliams07@gmail.com>
2020-07-10 13:16:31 -0500

    category resources, migration, and seeds

---
+++ db/migrate/20200710163315_create_categories.rb
@@ -0,0 +1,12 @@
+class CreateCategories < ActiveRecord::Migration[6.0]
+  def change
+    create_table :categories do |t|
+      t.string :name
+      t.string :description
+      t.string :img_url
+      t.string :instructions
+
+      t.timestamps
+    end
+  end
+end
